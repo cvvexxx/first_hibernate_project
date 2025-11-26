db6d3f3e26114488bffea67241f11160ac3108b1
cvvexxx <artem.potapov2307@gmail.com>
2025-11-26 15:16:42 +0300

    поменял main

--- src/main/java/cvv/project/Main.java
+++ src/main/java/cvv/project/Main.java
@@ -2,8 +2,7 @@ package cvv.project;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("popa");
-        System.out.println("1234");
+        System.out.println("Hello World!");
     }
 
     private static void startTimer() {
