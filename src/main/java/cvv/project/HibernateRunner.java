555b061c3ec10c679d4c4bb426aaa12f2cbaf536
cvvexxx <artem.potapov2307@gmail.com>
2025-11-27 20:34:10 +0300

    бб

--- src/main/java/cvv/project/HibernateRunner.java
+++ src/main/java/cvv/project/HibernateRunner.java
@@ -5,39 +5,48 @@ import cvv.project.convertor.BirthdayConvertor;
 import cvv.project.entity.Birthday;
 import cvv.project.entity.Role;
 import cvv.project.entity.User;
+import cvv.project.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 
 public class HibernateRunner {
+    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.configure();
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAttributeConverter(new BirthdayConvertor(), true);
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
 
-            User user = User.builder()
-                    .username("artem100@mail.ru")
-                    .firstname("Artem")
-                    .lastname("Potapov")
-                    .birthdate(new Birthday(LocalDate.of(2007, 6, 23)))
-                    .role(Role.USER)
-                    .build();
+        User user = User.builder()
+                .username("artem100@mail.ru")
+                .firstname("Artem")
+                .lastname("Potapov")
+                .birthdate(new Birthday(LocalDate.of(2007, 6, 23)))
+                .role(Role.USER)
+                .build();
+        log.info("User object in transient state: {}", user);
 
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+            try (Session session = sessionFactory.openSession()) {
+                session.beginTransaction();
+
+                user.setFirstname("Nikita");
+                log.warn("User firstname was changed: {}", user.getFirstname());
 //            session.persist(user);
 //            session.merge(user);
 //            session.remove(user);
 
-            User user1 = session.find(User.class, "artem@mail.ru");
-            User user2 = session.find(User.class, "artem1@mail.ru");
-            User user3 = session.find(User.class, "artem100@mail.ru");
+                User user1 = session.find(User.class, "artem@mail.ru");
+                User user2 = session.find(User.class, "artem1@mail.ru");
+                User user3 = session.find(User.class, "artem100@mail.ru");
 
-            session.getTransaction().commit();
+                session.getTransaction().commit();
+            }
+        } catch (Exception e) {
+            log.error("Exception in HibernateRunner: ", e);
+            throw e;
         }
     }
 }
--- src/main/java/cvv/project/util/HibernateUtil.java
+++ src/main/java/cvv/project/util/HibernateUtil.java
@@ -1,4 +1,16 @@
 package cvv.project.util;
 
+import cvv.project.convertor.BirthdayConvertor;
+import cvv.project.entity.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    public static SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAttributeConverter(new BirthdayConvertor(), true);
+        return configuration.buildSessionFactory();
+    }
 }
