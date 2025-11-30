391ef7f61e578d627ef09d534f99e7466dba0105
cvvexxx <artem.potapov2307@gmail.com>
2025-11-30 19:06:31 +0300

    поменял мапинг полей

--- src/main/java/cvv/project/entity/PersonalInfo.java
+++ src/main/java/cvv/project/entity/PersonalInfo.java
@@ -1,4 +1,23 @@
 package cvv.project.entity;
 
+import cvv.project.convertor.BirthdayConvertor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Embeddable
 public class PersonalInfo {
+    private String firstname;
+    private String lastname;
+    @Convert(converter = BirthdayConvertor.class)
+    @Column(name = "birth_date")
+    private Birthday birthdate;
 }
