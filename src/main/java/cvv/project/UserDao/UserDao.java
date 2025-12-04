14884e41e336695832be6e2db6966f64e3023c78
cvvexxx <artem.potapov2307@gmail.com>
2025-12-04 13:22:31 +0300

    deleted .log

--- src/main/java/cvv/project/UserDao/UserDao.java
+++ src/main/java/cvv/project/UserDao/UserDao.java
@@ -6,7 +6,6 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.hibernate.Session;
 
-import java.util.Collections;
 import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -25,7 +24,18 @@ public class UserDao {
 
         criteria.select(user);
 
-        return Collections.emptyList();
+        return session.createQuery(criteria).list();
     }
+
+    public List<User> findByFirstName(Session session, String firstName) {
+        var cb = session.getCriteriaBuilder();
+        var criteria = cb.createQuery(User.class);
+        var user = criteria.from(User.class);
+
+//        criteria.select(user).where(cb.equal(user.get(User_.personalInfo).get(PersonalInfo_.FIRSTNAME), firstName));
+
+        return session.createQuery(criteria).list();
+    }
+
 
 }
