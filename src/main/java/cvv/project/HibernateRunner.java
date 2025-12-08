f905e7691f05904b9374d1126149894b60015e20
cvvexxx <artem.potapov2307@gmail.com>
2025-12-08 13:44:24 +0300

    added base CriteriaAPI func

--- src/main/java/cvv/project/HibernateRunner.java
+++ src/main/java/cvv/project/HibernateRunner.java
@@ -16,71 +16,71 @@ public class HibernateRunner {
     private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
 
     public static void main(String[] args) {
-        User user1 = User.builder()
-                .username("john_doe")
-                .personalInfo(new PersonalInfo(
-                        "John",
-                        "Doe",
-                        new Birthday(LocalDate.of(1995, 3, 15))
-                ))
-                .role(Role.ADMIN)
-                .build();
-
-        User user2 = User.builder()
-                .username("jane_smith")
-                .personalInfo(new PersonalInfo(
-                        "Jane",
-                        "Smith",
-                        new Birthday(LocalDate.of(1998, 7, 2))
-                ))
-                .role(Role.USER)
-                .build();
-
-        User user3 = User.builder()
-                .username("alex_ivanov")
-                .personalInfo(new PersonalInfo(
-                        "Alex",
-                        "Ivanov",
-                        new Birthday(LocalDate.of(2000, 1, 20))
-                ))
-                .role(Role.USER)
-                .build();
-
-        User user4 = User.builder()
-                .username("maria_petrov")
-                .personalInfo(new PersonalInfo(
-                        "Maria",
-                        "Petrova",
-                        new Birthday(LocalDate.of(1993, 11, 5))
-                        ))
-                .role(Role.ADMIN)
-                .build();
-
-        User user5 = User.builder()
-                .username("sergey_kuznetsov")
-                .personalInfo(new PersonalInfo(
-                        "Sergey",
-                        "Kuznetsov",
-                        new Birthday(LocalDate.of(1999, 9, 9))
-                        ))
-                .role(Role.USER)
-                .build();
-
-        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
-            try (Session session = sessionFactory.openSession()) {
-                session.beginTransaction();
-
-                session.persist(user1);
-                session.persist(user2);
-                session.persist(user3);
-                session.persist(user4);
-                session.persist(user5);
-
-                session.getTransaction().commit();
-            }
-        } catch (Exception e) {
-            log.error("Exception in HibernateRunner: ", e);
-            throw e;
-        }
+//        User user1 = User.builder()
+//                .username("john_doe")
+//                .personalInfo(new PersonalInfo(
+//                        "John",
+//                        "Doe",
+//                        new Birthday(LocalDate.of(1995, 3, 15))
+//                ))
+//                .role(Role.ADMIN)
+//                .build();
+//
+//        User user2 = User.builder()
+//                .username("jane_smith")
+//                .personalInfo(new PersonalInfo(
+//                        "Jane",
+//                        "Smith",
+//                        new Birthday(LocalDate.of(1998, 7, 2))
+//                ))
+//                .role(Role.USER)
+//                .build();
+//
+//        User user3 = User.builder()
+//                .username("alex_ivanov")
+//                .personalInfo(new PersonalInfo(
+//                        "Alex",
+//                        "Ivanov",
+//                        new Birthday(LocalDate.of(2000, 1, 20))
+//                ))
+//                .role(Role.USER)
+//                .build();
+//
+//        User user4 = User.builder()
+//                .username("maria_petrov")
+//                .personalInfo(new PersonalInfo(
+//                        "Maria",
+//                        "Petrova",
+//                        new Birthday(LocalDate.of(1993, 11, 5))
+//                        ))
+//                .role(Role.ADMIN)
+//                .build();
+//
+//        User user5 = User.builder()
+//                .username("sergey_kuznetsov")
+//                .personalInfo(new PersonalInfo(
+//                        "Sergey",
+//                        "Kuznetsov",
+//                        new Birthday(LocalDate.of(1999, 9, 9))
+//                        ))
+//                .role(Role.USER)
+//                .build();
+//
+//        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+//            try (Session session = sessionFactory.openSession()) {
+//                session.beginTransaction();
+//
+//                session.persist(user1);
+//                session.persist(user2);
+//                session.persist(user3);
+//                session.persist(user4);
+//                session.persist(user5);
+//
+//                session.getTransaction().commit();
+//            }
+//        } catch (Exception e) {
+//            log.error("Exception in HibernateRunner: ", e);
+//            throw e;
+//        }
     }
 }
--- src/main/java/cvv/project/UserDao/UserDao.java
+++ src/main/java/cvv/project/UserDao/UserDao.java
@@ -1,7 +1,7 @@
 package cvv.project.UserDao;
 
 
-import cvv.project.entity.User;
+import cvv.project.entity.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.hibernate.Session;
@@ -32,10 +32,18 @@ public class UserDao {
         var criteria = cb.createQuery(User.class);
         var user = criteria.from(User.class);
 
-//        criteria.select(user).where(cb.equal(user.get(User_.personalInfo).get(PersonalInfo_.FIRSTNAME), firstName));
+        criteria.select(user).where(cb.equal(user.get(User_.personalInfo).get(PersonalInfo_.firstname), firstName));
 
         return session.createQuery(criteria).list();
     }
 
+    public List<Company> findCompanyByName(Session session, String companyName) {
+        var cb = session.getCriteriaBuilder();
+        var criteria = cb.createQuery(Company.class);
+        var company = criteria.from(Company.class);
 
+        criteria.select(company).where(cb.equal(company.get(Company_.name), companyName));
+
+        return session.createQuery(criteria).list();
+    }
 }
